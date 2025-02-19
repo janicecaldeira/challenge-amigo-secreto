06c0a0cc2aba776c79816ee80e0a22c85fba50c9
Janice Caldeira <janicecaldeira@gmail.com>
2025-02-19 10:16:04 -0300

    Added functions adicionarAmigo and atualizarListaAmigos

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const nomeAmigoInput = document.getElementById('amigo');
+    const nomeAmigo = nomeAmigoInput.value.trim();
+
+    if (nomeAmigo === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+    nomeAmigoInput.value = "";
+    atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+
+    amigos.forEach(function(amigo) {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
