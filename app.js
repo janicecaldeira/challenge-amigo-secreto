ec37eee614d2a7ad12b3c17247c7fc5af30a50e1
Janice Caldeira <janicecaldeira@gmail.com>
2025-02-19 10:20:37 -0300

    Added function sortearAmigo

--- app.js
+++ app.js
@@ -18,9 +18,23 @@ function atualizarListaAmigos() {
     const listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = "";
 
-    amigos.forEach(function(amigo) {
+    amigos.forEach(function (amigo) {
         const li = document.createElement('li');
         li.textContent = amigo;
         listaAmigos.appendChild(li);
     });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Não há amigos para sortear.");
+        return;
+    }
+
+    const indice = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indice];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Amigo sorteado: ${amigoSorteado}`;
 }
